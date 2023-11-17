0e178522043d7281a82c5f20711c615b8917bc79
FernandaGabrielli <123499117+FernandaGabrielli@users.noreply.github.com>
2023-11-17 01:57:09 -0300

    Update main.js
    Correção no toggle

--- main.js
+++ main.js
@@ -3,4 +3,10 @@ let navbar = document.querySelector('.navbar');
 
 menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('active');
+    toggleMenuIcon();
 });
+
+function toggleMenuIcon() {
+    menuIcon.classList.toggle('bx-menu');
+    menuIcon.classList.toggle('bx-x');
+}
